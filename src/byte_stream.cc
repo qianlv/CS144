ed38b40bc01f4a27018ee05bb8a0df116b1c4696
qianlv <qianlv7@qq.com>
2024-07-08 11:43:56 +0800

    check0: switch two buffers, example buffer 0 is empty when pop, swap buffer 0 and buffer 1

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,65 +1,94 @@
 #include "byte_stream.hh"
 
+#include <iostream>
+
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity )
+  : capacity_( capacity ), buffer_ { std::string( capacity_, '\0' ), std::string( capacity_, '\0' ) }
+{
+  // std::cerr << "capacity_ " << capacity_ << std::endl;
+}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return eof_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
-  return;
+  if ( is_closed() ) {
+    return;
+  }
+
+  uint64_t size = std::min( data.size(), available_capacity() );
+
+  total_pushed_bytes += size;
+
+  uint64_t first_size = 0;
+  if ( end_ < capacity_ ) {
+    first_size = std::min( capacity_ - end_, size );
+  }
+
+  if ( first_size > 0 ) {
+    std::copy( data.data(), data.data() + first_size, buffer_[0].data() + end_ );
+    end_ += first_size;
+  }
+
+  if ( size > first_size ) {
+    std::copy( data.data() + first_size, data.data() + size, buffer_[1].data() + ( end_ - capacity_ ) );
+    end_ += size - first_size;
+  }
 }
 
 void Writer::close()
 {
-  // Your code here.
+  eof_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_ - ( end_ - start_ );
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return total_pushed_bytes;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return eof_ && bytes_buffered() == 0;
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return total_poped_bytes;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  uint64_t size = bytes_buffered();
+  if ( size == 0 ) {
+    return "";
+  }
+  return std::string_view( buffer_[0].begin() + start_,
+                           buffer_[0].begin() + start_ + std::min( size, capacity_ - start_ ) );
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  len = std::min( len, bytes_buffered() );
+  start_ += len;
+  if ( start_ >= capacity_ ) {
+    end_ -= capacity_;
+    start_ -= capacity_;
+    std::swap( buffer_[0], buffer_[1] );
+  }
+  total_poped_bytes += len;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return end_ - start_;
 }
