9261f8280ab60b98957ea1dc1e18dd66355b3894
qianlv <qianlv7@qq.com>
2024-07-13 13:50:55 +0800

    (check2): 32bit wrap seqno and 64bit absolute seqno change

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,15 +1,45 @@
 #include "tcp_receiver.hh"
+#include "wrapping_integers.hh"
+#include <iostream>
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  (void)message;
+  if ( message.RST || reader().has_error() ) {
+    reader().set_error();
+    return;
+  }
+
+  auto seqno = message.seqno;
+  if ( message.SYN ) {
+    zero_point.emplace( message.seqno );
+    seqno = seqno + 1;
+  }
+
+  if ( !zero_point.has_value() ) {
+    return;
+  }
+
+  auto first_index = seqno.unwrap( *zero_point, writer().bytes_pushed() ) - 1;
+  reassembler_.insert( first_index, message.payload, message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  return {};
+  uint16_t window_size = UINT16_MAX;
+  if ( writer().available_capacity() < UINT16_MAX ) {
+    window_size = writer().available_capacity();
+  }
+
+  if ( !zero_point.has_value() ) {
+    return TCPReceiverMessage { std::nullopt, window_size, reader().has_error() };
+  }
+
+  TCPReceiverMessage ret;
+  ret.ackno = Wrap32::wrap( writer().bytes_pushed() + 1 + ( writer().is_closed() ? 1 : 0 ), *zero_point );
+  ret.window_size = window_size;
+  ret.RST = reader().has_error();
+
+  return ret;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,32 @@
 #include "wrapping_integers.hh"
+#include <iostream>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  n = ( n + zero_point.raw_value_ ) & UINT32_MAX;
+  return Wrap32 { static_cast<uint32_t>( n ) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  constexpr uint64_t UINT32_LEN { 1UL << 32 };
+  uint64_t offset = 0;
+  if ( raw_value_ < zero_point.raw_value_ ) {
+    offset = UINT32_LEN - static_cast<uint64_t>( zero_point.raw_value_ ) + static_cast<uint64_t>( raw_value_ );
+  } else {
+    offset = static_cast<uint64_t>( raw_value_ ) - static_cast<uint64_t>( zero_point.raw_value_ );
+  }
+
+  uint64_t nwrap = checkpoint >> 32;
+  // cerr << offset << " " << nwrap << " " << offset + nwrap * UINT32_LEN << " " << checkpoint << endl;
+  offset = offset + nwrap * UINT32_LEN;
+  if ( offset < checkpoint && ( offset + UINT32_LEN ) - checkpoint < checkpoint - offset ) {
+    return offset + UINT32_LEN;
+  } else if ( offset > checkpoint && offset > UINT32_LEN
+              && checkpoint - ( offset - UINT32_LEN ) < offset - checkpoint ) {
+    return offset - UINT32_LEN;
+  }
+  return offset;
 }
